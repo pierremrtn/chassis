6856a9a958890cfa874f82e4ffec66192f0ab95b
pierremrtn <pierre2tm@gmail.com>
2025-09-01 10:22:53 +0200

    simplify commands

--- chassis/lib/src/mediator/command.dart
+++ chassis/lib/src/mediator/command.dart
@@ -1,23 +1,16 @@
-abstract class Command<R> {}
-
-abstract interface class CommandHandler<C extends Command<R>, R> {
-  factory CommandHandler({required CommandHandlerCallback<C, R> run}) =
-      _CommandHandlerImpl.new;
-
-  Future<R> run(C command);
+abstract class Command<R> {
+  const Command();
 }
 
-typedef CommandHandlerCallback<C extends Command<R>, R> = Future<R> Function(C);
-
-class _CommandHandlerImpl<C extends Command<R>, R>
-    implements CommandHandler<C, R> {
-  const _CommandHandlerImpl({required CommandHandlerCallback<C, R> run})
+class CommandHandler<C extends Command<R>, R> {
+  const CommandHandler({required CommandHandlerCallback<C, R> run})
       : _callback = run;
 
   final CommandHandlerCallback<C, R> _callback;
 
-  @override
   Future<R> run(C command) {
     return _callback.call(command);
   }
 }
+
+typedef CommandHandlerCallback<C extends Command<R>, R> = Future<R> Function(C);
